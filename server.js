6033c0120b339800913daa7a761ad6ddb63e6878
awais <owaesmirza@gmail.com>
2018-04-21 15:34:00 +1000

    Chapter 4 Video : 7 Generation Token in back-end

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var jwt = require('jwt-simple');
 
 const port = 3000;
 
@@ -33,6 +34,27 @@ app.post('/register', (req, res) =>{
         }
     })
 });
+
+app.post('/login', async (req, res) =>{
+    let userData = req.body;
+    let user = await User.findOne({email: userData.email});
+
+    if(!user){
+        return res.status(401).send({message: 'Email or Password Invalid'})
+    }
+
+    if(userData.password != user.password){
+        return res.status(401).send({message: 'Email or Password Invalid'})
+    }
+
+    let payload = {}
+
+    let token = jwt.encode(payload, '123456')
+
+    res.status(200).send({token})
+});
+
+
 
 mongoose.connect('mongodb://awais:abc@ds147459.mlab.com:47459/meanstack',(err)=>{
     if(!err){
