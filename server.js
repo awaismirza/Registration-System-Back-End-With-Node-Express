7da841802a84ee192d1e2a620a9c12585a11f012
awais <owaesmirza@gmail.com>
2018-04-21 12:13:58 +1000

    Chapter3: Video 04 Saving User into MongoDB DataBase

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
 const port = 3000;
+
+var User = require('./models/user.js');
 
 var posts = [
     {message: 'Hello World'},
@@ -18,9 +22,21 @@ app.get('/posts', (req, res) =>{
 
 app.post('/register', (req, res) =>{
     let userData = req.body;
-    console.log(userData.email);
-    res.sendStatus(200);    
+    let user = new User(userData);
+
+    user.save((err, result) =>{
+        if(err){
+            console.log("Having trouble Adding User");
+        }else {
+            res.sendStatus(200);
+        }
+    })
 });
 
+mongoose.connect('mongodb://awais:abc@ds147459.mlab.com:47459/meanstack',(err)=>{
+    if(!err){
+        console.log('Connected to mongo Database');
+    }
+})
 
 app.listen(port);
